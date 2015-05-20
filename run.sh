9921d38978a2225b9d86c69f1bf11d8c50232aaf
murtex <kuberski@uni-potsdam.de>
2015-05-20 09:37:25 +0200

    added shell script for running matlab scripts

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+	# safeguard
+if [ "$#" -ne 1 ]; then
+	echo "usage: $0 SCRIPTFILE" >&2
+	exit 1
+fi
+
+if ! [ -e "$1" ]; then
+	echo "'$1' not found" >&2
+	exit 1
+fi
+
+	# run matlab script
+DIR=`dirname "$1"`
+FILE=`basename "$1"`
+
+cd "$DIR"
+matlab -nosplash -nodesktop -r "run ./$FILE; exit();"
+
