71c139baa2ee3b302e13427ae2e7ee9ac374c778
murtex <kuberski@uni-potsdam.de>
2015-06-21 00:55:12 +0200

    fixed a typo

--- cdf/+cdf/hConfig.m
+++ cdf/+cdf/hConfig.m
@@ -7,7 +7,7 @@ classdef (Sealed = true) hConfig < handle
 			% syncing
 		sync_range = [-0.075, 0.025]; % search range (row numeric)
 		sync_smooth = 0.0005; % smoothing (scalar numeric)
-		sync_thresh = 3; % mahalanobis threshold (scalar numeric)
+		sync_thresh = 3.0; % mahalanobis threshold (scalar numeric)
 
 			% voice activity detection
 		vad_frlength = 0.025; % short-time framing length (scalar numeric)
