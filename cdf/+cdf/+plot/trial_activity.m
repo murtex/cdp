210b118df98f8e060caeb6d455ba0e6ab9260a6a
murtex <kuberski@uni-potsdam.de>
2015-06-24 20:43:35 +0200

    updated trial activity plotting

--- cdf/+cdf/+plot/trial_activity.m
+++ cdf/+cdf/+plot/trial_activity.m
@@ -31,8 +31,82 @@ function trial_activity( run, cfg, id, plotfile )
 
 	style = xis.hStyle.instance();
 
+		% prepare data
+	trial = run.trials(id);
+
+	noir = dsp.sec2smp( [trial.range(1), trial.dist], run.audiorate ) + [1, 0]; % signal ranges
+	respr = dsp.sec2smp( trial.range, run.audiorate ) + [1, 0];
+
+	if any( isnan( noir ) ) || any( isnan( respr ) )
+		error( 'invalid value: noir | respr' );
+	end
+
+	noits = run.audiodata(noir(1):noir(2), 1); % signals
+	cdts = run.audiodata(respr(1):respr(2), 2);
+	respts = run.audiodata(respr(1):respr(2), 1);
+
+	frlen = dsp.sec2smp( cfg.vad_frlength, run.audiorate ); % short-time ffts
+	noifr = dsp.frame( noits, frlen, cfg.vad_froverlap, cfg.vad_frwindow );
+	respfr = dsp.frame( respts, frlen, cfg.vad_froverlap, cfg.vad_frwindow );
+	[noift, noifreqs] = dsp.fft( noifr, run.audiorate );
+	[respft, respfreqs] = dsp.fft( respfr, run.audiorate );
+	[noift, noifreqs] = dsp.band( noift, noifreqs, cfg.vad_freqband(1), cfg.vad_freqband(2), true ); % one-sided subband
+	[respft, respfreqs] = dsp.band( respft, respfreqs, cfg.vad_freqband(1), cfg.vad_freqband(2), true );
+
+	[respvafr, respsdfr, respddfr, respsdthresh, respddthresh] = k15.vad( ... % activity
+		respft, noift, cfg.vad_adjacency, cfg.vad_hangover );
+
+	respva = round( dsp.unframe( respvafr, frlen, cfg.vad_froverlap ) ); % unframing
+
+	xs = 1000 * dsp.smp2sec( 0:diff( respr ), run.audiorate ); % axes scaling
+	xsufr = 1000 * dsp.smp2sec( 0:numel( respva )-1, run.audiorate );
+	xsfr = 1000 * dsp.fr2sec( 0:size( respft, 2 )-1, frlen, cfg.vad_froverlap, run.audiorate );
+	xl = [min( xs ), max( xs )];
+	yl = max( abs( cat( 1, cdts, respts ) ) ) * style.width( 1/2 ) * [-1, 1];
+
 		% plot
 	fig = style.figure();
+
+	subplot( 4, 2, 1, 'XTickLabel', {} ); % cue/distractor signal
+	ylabel( 'cue/distractor' );
+	xlim( xl );
+	ylim( yl );
+	plot( xs, cdts, ...
+		'Color', style.color( 'cold', -1 ) );
+
+	subplot( 4, 2, 3, 'XTickLabel', {} ); % response signal
+	ylabel( 'response' );
+	xlim( xl );
+	ylim( yl );
+	plot( xsufr, respva * 0.5 * yl(2), ...
+		'Color', style.color( 'warm', 0 ) );
+	plot( xs, respts, ...
+		'Color', style.color( 'cold', -1 ) );
+
+	subplot( 4, 2, [5, 7] ); % spectrogram
+	xlabel( 'trial-time in milliseconds' );
+	ylabel( 'frequency in hertz' );
+	xlim( xl );
+	ylim( [min( respfreqs ), max( respfreqs )] );
+	colormap( style.gradient( 64, [1, 1, 1], style.color( 'cold', -2 ) ) );
+	imagesc( xsfr, respfreqs, log( respft .* conj( respft ) ) );
+
+	subplot( 4, 2, [2, 4], 'XTickLabel', {} ); % static distance
+	ylabel( 'static spectral distance' );
+	xlim( xl );
+	plot( xl, respsdthresh * [1, 1], ...
+		'Color', style.color( 'warm', 0 ) );
+	plot( xsfr, respsdfr, ...
+		'Color', style.color( 'cold', -1 ) );
+
+	subplot( 4, 2, [6, 8] ); % dynamic distance
+	xlabel( 'trial-time in milliseconds' );
+	ylabel( 'dynamic spectral distance' );
+	xlim( xl );
+	plot( xl, respddthresh * [1, 1], ...
+		'Color', style.color( 'warm', 0 ) );
+	plot( xsfr, respddfr, ...
+		'Color', style.color( 'cold', -1 ) );
 
 		% print
 	style.print( plotfile );
