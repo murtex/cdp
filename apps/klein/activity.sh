ff41a6f6ccf76e9a9d0fd20731072770e90ef837
murtex <kuberski@uni-potsdam.de>
2015-06-24 20:44:18 +0200

    updated activity detection/debugging

--- apps/klein/activity.sh
+++ apps/klein/activity.sh
@@ -5,25 +5,29 @@ INDIR="'../../data/klein-dev/sync/'"
 OUTDIR="'../../data/klein-dev/activity/'"
 
 	# spread workload
-matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 1:5 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 6:10 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 11:15 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 16:20 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 21:25 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 26:30 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 31:35 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 36:40 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 41:45 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 46:47 ); exit();" &
+#matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 1:5 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 6:10 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 11:15 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 16:20 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 21:25 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 26:30 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 31:35 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 36:40 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 41:45 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, 46:47 ); exit();" &
+
+	# DEBUG
+IDS="[15, 20]"
+matlab -nosplash -nodesktop -r "activity( $INDIR, $OUTDIR, $IDS ); exit();" &
 
 wait
 
---
+++ apps/klein/debug_activity.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+	# script arguments
+INDIR="'../../data/klein-dev/activity/'"
+OUTDIR="'../../data/klein-dev/debug-activity/'"
+NTRIALS=20
+SEED=1
+
+	# spread workload
+#matlab -nosplash -nodesktop -r "debug_activity( $INDIR, $OUTDIR, 1:5, $NTRIALS, $SEED ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug_activity( $INDIR, $OUTDIR, 6:10, $NTRIALS, $SEED ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug_activity( $INDIR, $OUTDIR, 11:15, $NTRIALS, $SEED ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug_activity( $INDIR, $OUTDIR, 16:20, $NTRIALS, $SEED ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug_activity( $INDIR, $OUTDIR, 21:25, $NTRIALS, $SEED ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug_activity( $INDIR, $OUTDIR, 26:30, $NTRIALS, $SEED ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug_activity( $INDIR, $OUTDIR, 31:35, $NTRIALS, $SEED ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug_activity( $INDIR, $OUTDIR, 36:40, $NTRIALS, $SEED ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug_activity( $INDIR, $OUTDIR, 41:45, $NTRIALS, $SEED ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug_activity( $INDIR, $OUTDIR, 46:47, $NTRIALS, $SEED ); exit();" &
+
+	# DEBUG
+IDS="[15, 20]"
+matlab -nosplash -nodesktop -r "debug_activity( $INDIR, $OUTDIR, $IDS, $NTRIALS, $SEED ); exit();" &
+
+wait
+
