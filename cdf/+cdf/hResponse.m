af8af8c7b751132d3807531691d4c314f78dfc4a
murtex <kuberski@uni-potsdam.de>
2015-07-12 18:14:16 +0200

    added landmarks

--- cdf/+cdf/hResponse.m
+++ cdf/+cdf/hResponse.m
@@ -7,6 +7,16 @@ classdef (Sealed = true) hResponse < handle
 			% activity
 		range = [NaN, NaN]; % voice activity range (row numeric)
 
+			% landmarks
+		bo = NaN; % burst-onset (scalar numeric)
+
+		f0 = [NaN, NaN]; % formant-onsets and frequencies (row numeric)
+		f1 = [NaN, NaN];
+		f2 = [NaN, NaN];
+		f3 = [NaN, NaN];
+
+		vr = NaN; % vowel-release (scalar numeric)
+
 	end
 
 end % classdef
