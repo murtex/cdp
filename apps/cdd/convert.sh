5cd935b21f01b95c9a0cec46e821b68bb2dca67c
murtex <kuberski@uni-potsdam.de>
2015-07-20 16:44:27 +0200

    updated shell scripts

--- apps/cdd/convert.sh
+++ apps/cdd/convert.sh
@@ -3,7 +3,11 @@
 	# script arguments
 INDIR="'../../data/cdd/raw/'"
 OUTDIR="'../../data/cdd/convert/'"
+
 IDS="0:40"
+
+	# prepare directories
+rm -rf $OUTDIR
 
 	# spread workload
 matlab -nosplash -nodesktop -r "convert( $INDIR, $OUTDIR, $IDS ); exit();" &
--- apps/klein/convert.sh
+++ apps/klein/convert.sh
@@ -3,7 +3,11 @@
 	# script arguments
 INDIR="'../../data/klein-dev/raw/'"
 OUTDIR="'../../data/klein-dev/convert/'"
+
 IDS="1:47"
+
+	# prepare directories
+rm -rf $OUTDIR
 
 	# spread workload
 matlab -nosplash -nodesktop -r "convert( $INDIR, $OUTDIR, $IDS ); exit();" &
