07d337a9a83b64536d936897eb25a9b7921604e5
murtex <kuberski@uni-potsdam.de>
2015-07-21 17:50:21 +0200

    updated application shell scripts

--- apps/klein/convert.sh
+++ apps/klein/convert.sh
@@ -5,15 +5,25 @@ INDIR='../../data/klein/raw/'
 OUTDIR='../../data/klein/convert/'
 
 	# spread workload
-matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 1:10 ); exit();" &
+matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 1:5 ); exit();" &
 sleep 3
-matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 11:20 ); exit();" &
+matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 6:10 ); exit();" &
 sleep 3
-matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 21:30 ); exit();" &
+matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 11:15 ); exit();" &
 sleep 3
-matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 31:40 ); exit();" &
+matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 16:20 ); exit();" &
 sleep 3
-matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 41:47 ); exit();" &
+matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 21:25 ); exit();" &
+sleep 3
+matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 26:30 ); exit();" &
+sleep 3
+matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 31:35 ); exit();" &
+sleep 3
+matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 36:40 ); exit();" &
+sleep 3
+matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 41:45 ); exit();" &
+sleep 3
+matlab -nosplash -nodesktop -r "convert( '$INDIR', '$OUTDIR', 46:47 ); exit();" &
 
 wait
 
--- apps/klein/process.sh
+++ apps/klein/process.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 	# landmark detection
-#./convert.sh
-#./sync.sh
-#./extract.sh
-#./landmark.sh
+./convert.sh
+./sync.sh
+./extract.sh
+./landmark.sh
 
 	# label classification
 #./features.sh
-./train.sh
-./classify.sh
+#./train.sh
+#./classify.sh
 
 	# debugging
 #./debug.sh
