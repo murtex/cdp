f3a0da6ab87e113ad85895838123ccb5a2fa287e
murtex <kuberski@uni-potsdam.de>
2015-07-21 18:01:31 +0200

    updated application shell scripts

--- apps/klein/classify.sh
+++ apps/klein/classify.sh
@@ -2,14 +2,14 @@
 
 	# data directories
 INDIR='../../data/klein/features/'
-OUTDIR='../../data/klein/classify/'
+OUTDIR='../../data/klein/classify-det/'
 TRAINDIR='../../data/klein/train/'
 
 	# spread workload
 IDS='setdiff( 1:47, 4 )'
 SEEDS='1:20'
 
-matlab -nosplash -nodesktop -r "classify( '$INDIR', '$OUTDIR', $IDS, '$TRAINDIR', $SEEDS); exit();" &
+matlab -nosplash -nodesktop -r "classify( '$INDIR', '$OUTDIR', $IDS, '$TRAINDIR', $SEEDS ); exit();" &
 
 wait
 
