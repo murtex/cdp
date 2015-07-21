10901e6379a71497bdae95ebc627c507831af063
murtex <kuberski@uni-potsdam.de>
2015-07-21 18:41:43 +0200

    updated application shell scripts

--- apps/klein/debug.sh
+++ apps/klein/debug.sh
@@ -1,29 +1,33 @@
 #!/bin/sh
 
 	# data directories
-INDIR='../../data/klein/classify/'
-OUTDIR='../../data/klein/debug/'
+INDIR='../../data/klein/classify-lab/'
+OUTDIR='../../data/klein/debug-worst/'
 
 	# spread workload
-matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 1:5 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 6:10 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 11:15 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 16:20 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 21:25 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 26:30 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 31:35 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 36:40 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 41:45 ); exit();" &
-sleep 3
-matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 46:47 ); exit();" &
+#matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 1:5 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 6:10 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 11:15 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 16:20 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 21:25 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 26:30 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 31:35 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 36:40 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 41:45 ); exit();" &
+#sleep 3
+#matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', 46:47 ); exit();" &
+
+	# DEBUG
+IDS='[]'
+matlab -nosplash -nodesktop -r "debug( '$INDIR', '$OUTDIR', $IDS ); exit();" &
 
 wait
 
--- apps/klein/process.sh
+++ apps/klein/process.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-	# landmark detection
+	# landmark detection (~10min)
 #./convert.sh
 #./sync.sh
 #./extract.sh
 #./landmark.sh
 
-	# label classification
-./features.sh
-./train.sh
-./classify.sh
+	# label classification (~25min)
+#./features.sh
+#./train.sh
+#./classify.sh
 
 	# debugging
-#./debug.sh
+./debug.sh
 
