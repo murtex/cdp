81b5368c1631eb40429ea9654349599b7efef9cd
System User <kuberski@maria.ling.uni-potsdam.de>
2015-08-20 08:30:40 +0200

    added conversion function

---
+++ cdf/+dsp/smp2msec.m
@@ -0,0 +1,26 @@
+function msec = smp2msec( smp, rate )
+% samples to milliseconds
+%
+% msec = SMP2MSEC( smp, rate )
+%
+% INPUT
+% smp : samples (numeric)
+% rate : sampling rate (scalar numeric)
+%
+% OUTPUT
+% msec : milliseconds (numeric)
+
+		% safeguard
+	if nargin < 1 || ~isnumeric( smp )
+		error( 'invalid argument: smp' );
+	end
+
+	if nargin < 2 || ~isscalar( rate ) || ~isnumeric( rate ) || rate <= 0
+		error( 'invalid argument: rate' );
+	end
+
+		% convert scale
+	msec = smp / rate * 1000;
+
+end
+
