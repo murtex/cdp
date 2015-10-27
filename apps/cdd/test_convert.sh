7062b5b498cdba8353c8af443dd71132e4680dfd
System User <kuberski@maria.ling.uni-potsdam.de>
2015-10-27 21:45:42 +0100

    added raw conversion statistics

---
+++ apps/cdd/test_convert.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+	# arguments
+INDIR="../../data/cdd/convert/"
+OUTDIR="${INDIR}/test/"
+
+IDS="setdiff( [1:40], 11 )" # 11th subject is malicious (two recordings)
+
+LOGFILE="${OUTDIR}/${IDS}.log"
+
+	# workload
+MATLAB="matlab -nosplash -nodesktop -r"
+
+$MATLAB "test.convert( '$INDIR', '$OUTDIR', $IDS, '$LOGFILE' ); exit();" &
+
+wait
+
