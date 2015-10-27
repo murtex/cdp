3b639632fea6f75e638a16159b3a60cd3555b9bc
System User <kuberski@maria.ling.uni-potsdam.de>
2015-10-27 21:14:04 +0100

    cleaned up

--- cdf/+cdf/hConfig.m
+++ cdf/+cdf/hConfig.m
@@ -9,17 +9,6 @@ classdef (Sealed = true) hConfig < handle
 		sync_smooth = NaN; % sample smoothing [approx. marker length] (scalar numeric)
 		sync_thresh = NaN; % mahalanobis threshold [sigmas] (scalar numeric)
 
-			% voice activity detection
-		vad_freqband = [150, 8000]; % frequency band [lower, upper] (row numeric)
-		vad_nfreqs = 200; % frequency resolution (scalar numeric)
-
-		vad_minlen = 0.075; % minimum activity length [klein: 0.082] (scalar numeric)
-
-		vad_maxdist = 1.5; % maximum distractor exposure [klein: 1.5042] (scalar numeric)
-		vad_maxgap = 0.1; % maximum shadowing gap (scalar numeric)
-
-		vad_maxdet = 2.2; % maximum detection length [klein: 2.038] (scalar numeric)
-
 	end
 
 end
