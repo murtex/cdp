904c48c4cf805a8ccfc3bb6edfd72b2c38addde2
System User <kuberski@maria.ling.uni-potsdam.de>
2015-11-09 22:54:10 +0100

    updated comments

--- cdf/+cdf/label_range.m
+++ cdf/+cdf/label_range.m
@@ -127,9 +127,9 @@ function label_range( run, cfg )
 			% prepare data
 		trial = run.trials(itrial);
 
-		tr = dsp.sec2smp( trial.range, run.audiorate ) + [1, 0]; % range
+		tr = dsp.sec2smp( trial.range, run.audiorate ) + [1, 0]; % ranges
 
-		rts = run.audiodata(tr(1):tr(2), 1); % signal
+		rts = run.audiodata(tr(1):tr(2), 1); % signals
 		if logscale
 			rts = log( abs( rts ) );
 		end
