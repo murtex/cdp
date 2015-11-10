89f39b7dcb6fa847e035d289ca370d7023ce5ced
System User <kuberski@maria.ling.uni-potsdam.de>
2015-11-10 20:54:41 +0100

    updated framework

--- cdf/+cdf/hConfig.m
+++ cdf/+cdf/hConfig.m
@@ -9,6 +9,10 @@ classdef (Sealed = true) hConfig < handle
 		sync_smooth = NaN; % sample smoothing [approx. marker length] (scalar numeric)
 		sync_thresh = NaN; % mahalanobis threshold [sigmas] (scalar numeric)
 
+			% manual labeling
+		lab_range_det1 = [-0.025, 0.05]; % range detail #1 [preceding, succeeding] (row numeric)
+		lab_range_det2 = [-0.05, 0.025]; % range detail #2 [preceding, succeeding] (row numeric)
+
 	end
 
 end
--- cdf/+cdf/hResponse.m
+++ cdf/+cdf/hResponse.m
@@ -6,16 +6,25 @@ classdef (Sealed = true) hResponse < handle
 
 			% activity
 		range = [NaN, NaN]; % activity range [start, stop] (row numeric)
+		range_uc = [NaN, NaN]; % uncertainty (row numeric)
 
 			% landmarks
-		bo = NaN; % burst-onset (scalar numeric)
-		vo = NaN; % voice-onset (scalar numeric)
+		bo = NaN; % burst onset (scalar numeric)
+		vo = NaN; % voice onset (scalar numeric)
+
+		bo_uc = NaN; % burst onset uncertainty (scalar numeric)
+		vo_uc = NaN; % voice onset uncertainty (scalar numeric)
 
 			% formants
 		f0 = [NaN, NaN]; % f0 onset [time, frequency] (row numeric)
 		f1 = [NaN, NaN]; % f1 onset [time, frequency] (row numeric)
 		f2 = [NaN, NaN]; % f2 onset [time, frequency] (row numeric)
 		f3 = [NaN, NaN]; % f3 onset [time, frequency] (row numeric)
+
+		f0_uc = [NaN, NaN]; % f0 onset uncertainty (row numeric)
+		f1_uc = [NaN, NaN]; % f1 onset uncertainty (row numeric)
+		f2_uc = [NaN, NaN]; % f2 onset uncertainty (row numeric)
+		f3_uc = [NaN, NaN]; % f3 onset uncertainty (row numeric)
 
 			% classification
 		label = ''; % response label (row char)
