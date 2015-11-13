9154a869a7a4eed735d9a5068a2e0091b8237d85
ampe <ampe@localhost.localdomain>
2015-11-13 01:56:35 +0100

    updated audio input

--- apps/cdd/+proc/read_audio.m
+++ apps/cdd/+proc/read_audio.m
@@ -25,7 +25,7 @@ function read_audio( run, audiofile )
 		% read audio file
 	run.audiofile = audiofile;
 
-	if exist( 'audioread', 'builtin' )
+	if exist( 'audioread' )
 		[run.audiodata, run.audiorate] = audioread( audiofile );
 	else
 		[run.audiodata, run.audiorate] = wavread( audiofile );
