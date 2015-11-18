f15305aa22148e925e409cc35db28b488f86d8ea
System User <kuberski@maria.ling.uni-potsdam.de>
2015-11-18 21:49:32 +0100

    added landmarks auditing tool

---
+++ cdf/+cdf/audit_landmarks.m
@@ -0,0 +1,288 @@
+function audit_landmarks( run, cfg )
+% landmarks auditing tool
+%
+% AUDIT_LANDMARKS( run, cfg )
+% 
+% INPUT
+% run : cue-distractor run (scalar object)
+% cfg : framework configuration (scalar object)
+
+		% safeguard
+	if nargin < 1 || ~isscalar( run ) || ~isa( run, 'cdf.hRun' ) % cue-distractor run
+		error( 'invalid argument: run' );
+	end
+
+	if nargin < 2 || ~isscalar( cfg ) || ~isa( cfg, 'cdf.hConfig' ) % framework configuration
+		error( 'invalid argument: cfg' );
+	end
+
+		% init
+	logger = xis.hLogger.instance(); % start logging
+	logger.tab( 'landmarks auditing tool...' );
+
+	style = xis.hStyle.instance(); % prepare interactive figure
+
+	fig = style.figure( 'Visible', 'on' );
+
+	set( fig, 'WindowKeyPressFcn', {@fig_dispatch, 'keypress'} );
+	set( fig, 'CloseRequestFcn', {@fig_dispatch, 'close'} );
+
+		% helper functions
+	function f = is_valid( trials )
+		f = true( size( trials ) );
+		for i = 1:numel( trials )
+			if any( isnan( trials(i).resplab.range ) )
+				f(i) = false;
+			end
+		end
+	end
+
+	function i = nearest_zc( ts, t0, i )
+		if cfg.lab_activity_zcalign
+
+			zc = sign( ts ); % find zero crossings
+			zc(zc == 0) = 1;
+			zc = abs( diff( zc ) / 2 );
+			zc = find( zc == 1 );
+
+			if isempty( zc )
+				return;
+			end
+
+			is = dsp.sec2smp( i - t0, run.audiorate ) + 1; % choose nearest
+			d = zc - is;
+			d = d(find( abs( d ) == min( abs( d ) ), 1 )) + 1;
+			i = dsp.smp2sec( is + d - 1, run.audiorate ) + t0;
+
+		end
+	end
+
+	function plot_landmarks( trial, yl )
+		plot( (trial.resplab.bo * [1, 1] - trial.range(1)) * 1000, yl, ...
+			'Color', style.color( 'signal', +1 ) );
+		plot( (trial.resplab.vo * [1, 1] - trial.range(1)) * 1000, yl, ...
+			'Color', style.color( 'signal', +1 ) );
+	end
+
+		% event dispatching
+	function fig_dispatch( src, event, type )
+		switch type
+
+				% key presses
+			case 'keypress'
+				nmods = size( event.Modifier, 2 );
+
+				switch event.Key
+
+					case 'pagedown' % trial browsing
+						if nmods < 2 && itrial ~= ntrials
+							step = 1;
+							if strcmp( event.Modifier, 'shift' )
+								step = 10;
+							elseif strcmp( event.Modifier, 'control' )
+								step = 100;
+							end
+							itrial = min( itrial + step, ntrials );
+							fig_update();
+						end
+					case 'pageup'
+						if nmods < 2 && itrial ~= 1
+							step = 1;
+							if strcmp( event.Modifier, 'shift' )
+								step = 10;
+							elseif strcmp( event.Modifier, 'control' )
+								step = 100;
+							end
+							itrial = max( itrial - step, 1 );
+							fig_update();
+						end
+
+					case 'home'
+						if nmods == 0 && itrial ~= 1
+							itrial = 1;
+							fig_update();
+						end
+					case 'end'
+						if nmods == 0 && itrial ~= ntrials
+							itrial = ntrials;
+							fig_update();
+						end
+
+					case 'return' % playback
+						if nmods == 0
+							sound( ovrts / max( abs( ovrts ) ), run.audiorate );
+						elseif nmods == 1 && strcmp( event.Modifier, 'shift' ) && fresp
+							sound( respts / max( abs( respts ) ), run.audiorate );
+						end
+
+					case 'escape' % quit
+						if nmods == 0
+							done = true;
+							fig_update();
+						end
+
+					otherwise % DEBUG
+						logger.log( 'keypress: %s', event.Key );
+				end
+
+				% figure closing
+			case 'close'
+				done = true;
+				delete( fig );
+		end
+	end
+
+	function fig_update()
+		switch get( fig, 'Clipping' ) % flip (unused) clipping property
+			case 'on'
+				set( fig, 'Clipping', 'off' );
+			case 'off'
+				set( fig, 'Clipping', 'on' );
+		end
+	end
+
+		% interaction loop
+	trials = [run.trials]; % prepare trials
+	trials(~is_valid( trials )) = [];
+
+	ntrials = numel( trials );
+	logger.log( 'valid trials: %d', ntrials );
+
+	if ntrials == 0
+		error( 'invalid value: ntrials' );
+	end
+
+	itrial = 1;
+
+	done = false; % init flags
+
+	while ~done
+
+			% prepare data
+		trial = trials(itrial);
+		resp = trial.resplab;
+
+		ovrr = dsp.sec2smp( resp.range, run.audiorate ) + [1, 0]; % ranges
+
+		respr = dsp.sec2smp( [resp.bo, resp.range(2)], run.audiorate ) + [1, 0];
+		fresp = ~any( isnan( respr ) );
+
+		det1r = dsp.sec2smp( resp.bo + cfg.lab_landmarks_det1, run.audiorate ) + [1, 0];
+		det1r(det1r < 1) = 1;
+		det1r(det1r > size( run.audiodata, 1 )) = size( run.audiodata, 1 );
+		fdet1 = ~any( isnan( det1r ) );
+
+		det2r = dsp.sec2smp( resp.vo + cfg.lab_landmarks_det2, run.audiorate ) + [1, 0];
+		det2r(det2r < 1) = 1;
+		det2r(det2r > size( run.audiodata, 1 )) = size( run.audiodata, 1 );
+		fdet2 = ~any( isnan( det2r ) );
+
+		ovrts = run.audiodata(ovrr(1):ovrr(2), 1); % signals
+
+		respts = [];
+		if fresp
+			respts = run.audiodata(respr(1):respr(2), 1);
+		end
+
+		det1ts = [];
+		if fdet1
+			det1ts = run.audiodata(det1r(1):det1r(2), 1);
+		end
+
+		det2ts = [];
+		if fdet2
+			det2ts = run.audiodata(det2r(1):det2r(2), 1);
+		end
+
+			% plot
+		clf( fig );
+
+		subplot( 4, 2, [1, 2] ); % overview
+		title( sprintf( 'LABEL_LANDMARKS (trial: %d/%d)', itrial, ntrials ) );
+		xlabel( 'trial time in milliseconds' );
+		ylabel( 'response' );
+
+		xlim( (resp.range - trial.range(1)) * 1000 );
+		yl = max( abs( ovrts ) ) * [-1, 1] * style.scale( 1/2 );
+		ylim( yl );
+
+		plot_landmarks( trial, yl ); % landmarks
+
+		plot( (dsp.smp2sec( (ovrr(1):ovrr(2)) - 1, run.audiorate ) - trial.range(1)) * 1000, ovrts, ... % signal
+			'Color', style.color( 'cold', -1 ) );
+
+		if fdet1 % detail #1 (burst onset)
+			subplot( 4, 2, [3, 5] );
+			xlabel( 'trial time in milliseconds' );
+			ylabel( 'burst onset detail' );
+
+			xlim( (resp.bo + cfg.lab_landmarks_det1 - trial.range(1)) * 1000 );
+			yl = max( abs( det1ts ) ) * [-1, 1] * style.scale( 1/2 );
+			ylim( yl );
+
+			plot_landmarks( trial, yl ); % landmarks
+
+			plot( (dsp.smp2sec( (det1r(1):det1r(2)) - 1, run.audiorate ) - trial.range(1)) * 1000, det1ts, ... % signal
+				'Color', style.color( 'cold', -1 ) );
+		end
+
+		if fdet2 % detail #2 (voice onset)
+			subplot( 4, 2, [4, 6] );
+			xlabel( 'trial time in milliseconds' );
+			ylabel( 'voice onset detail' );
+
+			xlim( (resp.vo + cfg.lab_landmarks_det2 - trial.range(1)) * 1000 );
+			yl = max( abs( det2ts ) ) * [-1, 1] * style.scale( 1/2 );
+			ylim( yl );
+
+			plot_landmarks( trial, yl ); % landmarks
+
+			plot( (dsp.smp2sec( (det2r(1):det2r(2)) - 1, run.audiorate ) - trial.range(1)) * 1000, det2ts, ... % signal
+				'Color', style.color( 'cold', -1 ) );
+		end
+
+		s = { ... % information
+			'INFORMATION', ...
+			'', ...
+			sprintf( 'class: ''%s''', resp.label ), ...
+			'', ...
+			sprintf( 'activity: [%.1f, %.1f]', (resp.range - trial.range(1)) * 1000 ), ...
+			'', ...
+			sprintf( 'burst onset: %.1f', (resp.bo - trial.range(1)) * 1000 ), ...
+			sprintf( 'voice onset: %.1f', (resp.vo - trial.range(1)) * 1000 ), ...
+			'', ...
+			sprintf( 'F0 onset: [%.1f, %.1f]', (resp.f0 - [trial.range(1), 0]) .* [1000, 1] ), ...
+			sprintf( 'F1 onset: [%.1f, %.1f]', (resp.f1 - [trial.range(1), 0]) .* [1000, 1] ), ...
+			sprintf( 'F2 onset: [%.1f, %.1f]', (resp.f2 - [trial.range(1), 0]) .* [1000, 1] ), ...
+			sprintf( 'F3 onset: [%.1f, %.1f]', (resp.f3 - [trial.range(1), 0]) .* [1000, 1] ) };
+
+		annotation( 'textbox', [0, 0, 1/3, 1/4], 'String', s );
+
+		s = { ... % commands
+			'COMMANDS', ...
+			'', ...
+			'PAGEDOWN/UP: +/- 1 trial' ...
+			'SHIFT+PAGEDOWN/UP: +/- 10 trials', ...
+			'CONTROL+PAGEDOWN/UP: +/- 100 trials', ...
+			'HOME/END: first/last trial', ...
+			'', ...
+			'RETURN: playback audio', ...
+			'', ...
+			'ESCAPE: quit' };
+
+		annotation( 'textbox', [2/3, 0, 1/3, 1/4], 'String', s );
+
+			% wait for figure update
+		waitfor( fig, 'Clipping' ); % wait for (unused) clipping property change
+
+	end
+
+		% exit
+	if ishandle( fig )
+		delete( fig );
+	end
+
+	logger.untab(); % stop logging
+
+end
+
