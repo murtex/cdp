2358c11c40c60e45e990077a59be51948be89091
ampe <kuberski@uni-potsdam.de>
2015-12-02 19:37:20 +0100

    updated comment

--- cdf/+cdf/hConfig.m
+++ cdf/+cdf/hConfig.m
@@ -11,12 +11,12 @@ classdef (Sealed = true) hConfig < handle
 
 			% labeling
 		lab_activity_det1 = [NaN, NaN]; % (relative) activity detail #1 (start) [start, stop] (row numeric)
-		lab_activity_det2 = [NaN, NaN]; % (relative) activity detail #2 (stop) [start, stop] (row numeric)
+		lab_activity_det2 = [NaN, NaN]; % (relative) detail #2 (stop) [start, stop] (row numeric)
 		lab_activity_zcsnap = [false, false]; % zero-crossings alignment [det1, det2] (logical scalar)
 
 		lab_landmarks_det1 = [NaN, NaN]; % (relative) landmarks detail #1 (burst onset) [start, stop] (row numeric)
-		lab_landmarks_det2 = [NaN, NaN]; % (relative) landmarks detail #2 (voice onset) [start, stop] (row numeric)
-		lab_landmarks_det3 = [NaN, NaN]; % (relative) landmarks detail #3 (voice voice release) [start, stop] (row numeric)
+		lab_landmarks_det2 = [NaN, NaN]; % (relative) detail #2 (voice onset) [start, stop] (row numeric)
+		lab_landmarks_det3 = [NaN, NaN]; % (relative) detail #3 (voice voice release) [start, stop] (row numeric)
 		lab_landmarks_zcsnap = [false, false, false]; % zero-crossings alignment [det1, det2, det3] (row logical)
 
 		lab_formants_f0_freqband = [NaN, NaN, NaN]; % f0 frequency band [lower, upper, count] (row numeric)
@@ -28,11 +28,11 @@ classdef (Sealed = true) hConfig < handle
 
 			% auditing
 		aud_activity_det1 = [NaN, NaN]; % (relative) activity detail #1 (start) [start, stop] (row numeric)
-		aud_activity_det2 = [NaN, NaN]; % (relative) activity detail #2 (stop) [start, stop] (row numeric)
+		aud_activity_det2 = [NaN, NaN]; % (relative) detail #2 (stop) [start, stop] (row numeric)
 
 		aud_landmarks_det1 = [NaN, NaN]; % (relative) landmarks detail #1 (burst onset) [start, stop] (row numeric)
-		aud_landmarks_det2 = [NaN, NaN]; % (relative) landmarks detail #2 (voice onset) [start, stop] (row numeric)
-		aud_landmarks_det3 = [NaN, NaN]; % (relative) landmarks detail #3 (voice release) [start, stop] (row numeric)
+		aud_landmarks_det2 = [NaN, NaN]; % (relative) detail #2 (voice onset) [start, stop] (row numeric)
+		aud_landmarks_det3 = [NaN, NaN]; % (relative) detail #3 (voice release) [start, stop] (row numeric)
 
 		aud_formants_f0_freqband = [NaN, NaN, NaN]; % f0 frequency band [lower, upper, count] (row numeric)
 		aud_formants_f0_window = {@rectwin, NaN, NaN}; % short-time window [function, length, overlap] (row cell)
